7189c5c86c5e38acf11e698024295659e16b0371
Nkateko Shikuhele <shikuhelenkateko@gmail.com>
2025-04-13 00:36:28 +0200

    add a todo class constructor

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 // index.js
 import "./styles.css";
-import { todo } from "./todo.js";
+// import { todo } from "./todo.js";
+// console.log(todo);
 
-console.log(todo);
+class todos {
+  constructor(title, description, dueDate, priority, notes, checklist) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.notes = notes;
+    this.checklist = checklist;
+  }
+
+  submitTodo() {
+    
+  }
+
+  deleteTodo() {
+    console.log("tod deleted")
+  }
+
+  editTodo() {
+    console.log("Edit todo")
+  }
+}
